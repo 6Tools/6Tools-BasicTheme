78dabd9b51b589c83b303fe8f984fea32c6957c0
Gaetan mOveo <gaetan@moveo-webdesign.com>
2013-10-31 15:24:00 +0100

    Modification du footer

--- footer.php
+++ footer.php
@@ -9,12 +9,60 @@
  * @since Foundation, for WordPress 1.0
  */
 ?>
-</div><!-- #end right content -->
-</div><!-- #end main grid -->
+<!-- Footer -->
+<div class="footer--content">
+    <div class="inner-footer grid grid--center grid--table">
+        <div class="grid__item one-quarter">
+            <ul class="nav nav--stacked nav--stacked--center">
+                <li class="nav--title">Présentation</li>
+                <li><a href="#">Element court</a></li>
+                <li><a href="#">Element de menu vertical</a></li>
+                <li><a href="#">Element court</a></li>
+                <li><a href="#">Element long de menu</a></li>
+            </ul>
+        </div><!-- 
+        --><div class="grid__item one-quarter">
+            <ul class="nav nav--stacked nav--stacked--center">
+                <li class="nav--title">Services</li>
+                <li><a href="#">Element de menu vertical</a></li>
+                <li><a href="#">Element long de menu</a></li>
+                <li><a href="#">Element de menu classique</a></li>
+                <li><a href="#">Element court</a></li>
+            </ul>
+        </div><!-- 
+        --><div class="grid__item one-quarter">
+            <ul class="nav nav--stacked nav--stacked--center">
+                <li class="nav--title">réalisations</li>
+                <li><a href="#">Element de menu vertical</a></li>
+                <li><a href="#">Element de menu classique</a></li>
+                <li><a href="#">Element court</a></li>
+                <li><a href="#">Element long de menu</a></li>
+            </ul>
+        </div><!-- 
+        --><div class="grid__item one-quarter">
+            <ul class="nav nav--stacked nav--stacked--center">
+                <li class="nav--title">contact</li>
+                <li><a href="#">Element long de menu</a></li>
+                <li><a href="#">Element court</a></li>
+                <li><a href="#">Element de menu vertical</a></li>
+                <li><a href="#">Element de menu classique</a></li>
+                <li><a href="#">Element de menu classique</a></li>
+                <li><a href="#">Element de menu classique</a></li>
+            </ul>
+        </div>
+    </div>
+    
+</div>
 
-<?php if(my_wp_is_mobile()): ?>
-    <hr class="separator--double no-margin always-bottom"/>
-<?php endif; ?>
+<footer class="footer--baseline">
+    <div class="inner-footer grid grid--center">
+        <div class="grid__item one-whole justify-blocks">
+            <p class="justify-blocks__item">2013 © 6TOOLS - Tous droits réservés - <a href="#">Mentions légales</a> - <a href="#">Plan du site</a> - <a href="#">Partenaires</a> - <a href="#">Contact</a></p>
+            <p class="justify-blocks__item">Réalisation : <a href="#">six design</a></p>
+        </div>
+    </div>
+</footer>
+<!-- End Footer -->
  <!--[if lt IE 9]>
    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/libs/respond.min.js"></script>
    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery.placeholder.js"></script>
